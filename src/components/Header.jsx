14ecd392512a3bf1f998684af0e9ea4eab2e3e08
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2024-11-13 10:02:32 -0300

    feat: added styles in the <a/>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,15 +8,15 @@ const Header = () => {
             <img src={logo} alt="LoCarros logo" className='w-[280px]' />
 
             <div className='flex items-center justify-center gap-10'>
-            <li className='text-black font-light list-none'>
+            <li className='text-black font-normal list-none border-b-2 border-transparent hover:border-red-500 transition-all duration-300'>
                 <a href="/inicio">Início</a>
             </li>
 
-            <li className='text-black font-light list-none'>
+            <li className='text-black font-normal list-none border-b-2 border-transparent hover:border-red-500 transition-all duration-300'>
                 <a href="/carros">Carros</a>
             </li>
 
-            <li className='text-black font-light list-none'>
+            <li className='text-black font-normal list-none border-b-2 border-transparent hover:border-red-500 transition-all duration-300'>
                 <a href="/locador">Locador</a>
             </li>
             </div>
